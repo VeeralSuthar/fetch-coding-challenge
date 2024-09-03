8f1ec71fd246d1da2a549fa1f94ceab7f590129e
Veeral Suthar <vsuthar@oai.aero>
2024-09-03 12:15:03 -0500

    Initial Commit

---
+++ fetch/fetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchApp.swift
+//  fetch
+//
+//  Created by Veeral Suthar on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct fetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
